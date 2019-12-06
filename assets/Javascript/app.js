bdd8751a80bc3fc9179f4d38b3b3fa1b91365754
Derek Bennett <bnnder@netscape.net>
2019-12-05 17:12:49 -0800

    added the firebase config path and variables in the app.js file

---
+++ assets/Javascript/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC-xq_xfvfQBBz8I5JNDn7yXN3297Kwpn4",
+    authDomain: "fire-project-7f704.firebaseapp.com",
+    databaseURL: "https://fire-project-7f704.firebaseio.com",
+    projectId: "fire-project-7f704",
+    storageBucket: "fire-project-7f704.appspot.com",
+    messagingSenderId: "17943389109",
+    appId: "1:17943389109:web:d3b95edc0a407fe4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// Variable for Firebase database
+var database = firebase.database();
+
+var trainName;
+var destination;
+var startTime;
+var frequency = 0;
+
+
+});
